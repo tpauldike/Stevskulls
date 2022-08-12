8257198d41e43b06aeac3d6bfdb3fc8c1277c5a9
tpauldike <topman4loveworld@gmail.com>
2022-08-12 03:34:24 +0000

    Day 1

---
+++ add2num.c
@@ -0,0 +1,16 @@
+/*A program written to explain C language basically*/
+#include <stdio.h>
+
+/**
+  * main - adds two integers and prints the sum
+  * Return: 0
+  */
+int main(void)
+{
+	/* declaration of variable */
+	int Stev = 3, Efe = 6, Topman = Stev + Efe;
+
+	printf("Using shorter line of code\n%i + %d = %d\n", Stev, Efe, Topman);
+
+	return (0);
+}
